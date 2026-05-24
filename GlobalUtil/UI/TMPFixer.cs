27098f4c110753985fe7b6193c49db90cee657bb
ttdly <ttdlyu@163.com>
2026-05-24 15:51:14 +0800

    feat(ui): 添加部分 UI 相关代码
    大部分代码来自 https://github.com/aki-art/ONI-Mods，已标注来源

---
+++ GlobalUtil/UI/TMPFixer.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GlobalUtil.UI
+{
+    /// <summary>
+    ///     Credit: Aki
+    /// </summary>
+    // There is an issue with how TMP imports itself and alignment has to be reapplied
+    internal class TMPFixer : KMonoBehaviour {
+        [SerializeField]
+        public TextAlignmentOptions alignment;
+
+        [MyCmpReq] private LocText text;
+
+        protected override void OnSpawn() {
+            base.OnSpawn();
+            text.alignment = alignment;
+            Destroy(this);
+        }
+    }
+}
---
+++ GlobalUtil/UI/Util.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace GlobalUtil.UI
+{
+    public class Util
+    {
+        /// <summary>
+        ///     Credit: Sgt_Imalas
+        /// </summary>
+        public static AssetBundle LoadAssetBundle(string assetBundleName, string path = null,
+            bool platformSpecific = false) {
+            foreach (var bundle in AssetBundle.GetAllLoadedAssetBundles())
+                if (bundle.name == assetBundleName)
+                    return bundle;
+
+            if (path.IsNullOrWhiteSpace()) {
+                var exec = Assembly.GetExecutingAssembly().Location;
+                var execDir = Path.GetDirectoryName(exec);
+                if (execDir == null) throw new Exception($"Could not find executable at {exec}.");
+
+                path = Path.Combine(execDir, "assets");
+            }
+
+            if (platformSpecific) {
+                if (path == null) throw new Exception($"Could not find asset dir at {assetBundleName}.");
+                switch (Application.platform) {
+                    case RuntimePlatform.WindowsPlayer:
+                        path = Path.Combine(path, "windows");
+                        break;
+                    case RuntimePlatform.LinuxPlayer:
+                        path = Path.Combine(path, "linux");
+                        break;
+                    case RuntimePlatform.OSXPlayer:
+                        path = Path.Combine(path, "mac");
+                        break;
+                    default:
+                        throw new Exception($"Not supported platform: {Application.platform}");
+                }
+            }
+
+            if (path == null) throw new Exception($"Could not find platform dir at {path}.");
+
+            path = Path.Combine(path, assetBundleName);
+
+            var assetBundle = AssetBundle.LoadFromFile(path);
+
+            if (assetBundle != null) return assetBundle;
+
+            Logger.Warning($"Failed to load AssetBundle from path {path}");
+            return null;
+        }
+
+        /// <summary>
+        ///     Credit: Sgt_Imalas
+        /// </summary>
+        public static void ListChildren(Transform parent, int level = 0, int maxDepth = 10) {
+            if (level >= maxDepth) return;
+
+            foreach (Transform child in parent) {
+                Logger.Info(string.Concat(Enumerable.Repeat('-', level)) + child.name);
+                ListChildren(child, level + 1);
+            }
+        }
+    }
+}
